5cc43b371fff72ac5cef30c160eccc1d0ef61e2d
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-10-20 16:13:38 -0200

    Adição de comentários

--- gerarBoard.lua
+++ gerarBoard.lua
@@ -1,12 +1,14 @@
-function contains(num, tabela)
+--Função para verificar se uma tabela contém um certo valor
+function contains(valor, tabela)
 	for i = 1, #tabela do
-		if tabela[i] == num then
+		if tabela[i] == valor then
 			return true
 		end
 	end
 	return false
 end
 
+--Função para remover um item da tabela pelo seu valor
 function removeItem(tabela, valor)
 	for i = 1, #tabela do
 		if tabela[i] == valor then
@@ -17,14 +19,18 @@ function removeItem(tabela, valor)
 end
 
 function gerarRandomNum(sudoku, index)
+	--Números possíveis para inserir no board
 	local candidatos = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
+	--Identifica as coordenadas pelo índice passado
 	local linha = math.floor(index/9)
 	local coluna = index % 9
 
+	--Identifica a submatriz onde se encontra o índice
 	local grid_x = math.floor(linha / 3) * 3
 	local grid_y = math.floor(coluna / 3) * 3
 
+	--Confere os números da linha e remove da tabela os números já inseridos
 	local j = 0
 	while j < coluna do
 		if contains(sudoku[linha * 9 + j + 1], candidatos) then
@@ -33,6 +39,7 @@ function gerarRandomNum(sudoku, index)
 		j = j + 1
 	end
 
+	--Confere os números da coluna e remove da tabela os números já inseridos
 	local i = 0
 	while i < linha do
 		if contains(sudoku[i * 9 + coluna + 1], candidatos) then
@@ -41,6 +48,7 @@ function gerarRandomNum(sudoku, index)
 		i = i + 1
 	end
 
+	--Confere os números da submatriz e remove da tabela os números já inseridos
 	for _i = 0, 2 do
 		for _j = 0, 2 do
 			if contains(sudoku[(grid_x + _i) * 9 + grid_y + _j + 1], candidatos) then
@@ -49,8 +57,10 @@ function gerarRandomNum(sudoku, index)
 		end
 	end
 
+	--Se não houver um número válido para ser inserido retorna -1 e zera a tabela
 	if #candidatos == 0 then
 		randomNum = -1
+	-- Se houver pelo menos um número dentro da tabela, seleciona um aleatóriamente e o remove da tabela
 	else
 		randomNum = candidatos[math.random(#candidatos)]
 		removeItem(candidatos, randomNum)
@@ -59,12 +69,14 @@ function gerarRandomNum(sudoku, index)
 end
 
 function gerarBoard()
+	-- Cria um board zerado
 	sudoku = {}
 	for i = 1, 81 do
 		table.insert(sudoku, 0)
 	end
 
 	i = 0
+	--Insere números aleatórios na tabela. Caso não exista um número válido, a tabela é zerada
 	while i < 81 do
 		randomNum = gerarRandomNum(sudoku, i)
 		if randomNum == -1 then
