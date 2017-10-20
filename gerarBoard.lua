6f6270f957a7243b62284de53c91bfaaa01ba3e4
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-10-20 15:35:29 -0200

    Correção dos índices. Programa ainda está com alguns bugs

--- gerarBoard.lua
+++ gerarBoard.lua
@@ -1,40 +1,50 @@
 function contains(num, tabela)
 	for i = 1, #tabela do
-		if tabela[i] == num
+		if tabela[i] == num then
 			return true
+		end
 	end
 	return false
 end
 
+function removeItem(tabela, valor)
+	for i = 1, #tabela do
+		if tabela[i] == valor then
+			table.remove(tabela, i)
+			return
+		end
+	end
+end
+
 function gerarRandomNum(sudoku, index)
-	candidatos = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-	
-	linha = index / 9
-	coluna = index % 9
+	local candidatos = {1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	grid_x = linha / 3 * 3
-	grid_y = coluna / 3 * 3
+	local linha = math.floor(index/9)
+	local coluna = index % 9
 
-	j = 0
+	local grid_x = math.floor(linha / 3) * 3
+	local grid_y = math.floor(coluna / 3) * 3
+
+	local j = 0
 	while j < coluna do
-		if contains(sudoku[linha * 9 + j], candidatos) then
-			table.remove(candidatos, sudoku[linha * 9 + j])
+		if contains(sudoku[linha * 9 + j + 1], candidatos) then
+			removeItem(candidatos, sudoku[linha * 9 + j + 1])
 		end
 		j = j + 1
 	end
 
-	i = 0
+	local i = 0
 	while i < linha do
-		if contains(sudoku[i * 9 + coluna], candidatos) then
-			table.remove(candidatos, sudoku[i * 9 + coluna])
+		if contains(sudoku[i * 9 + coluna + 1], candidatos) then
+			removeItem(candidatos, sudoku[i * 9 + coluna + 1])
 		end
 		i = i + 1
 	end
 
-	for _i = 0, 3 do
-		for _j = 0, 3 do
-			if contains(sudoku[(grid_x + _i) * 9 + grid_y + _j], candidatos) then
-				table.remove(candidatos, sudoku[(grid_x + _i) * 9 + grid_y + _j])
+	for _i = 0, 2 do
+		for _j = 0, 2 do
+			if contains(sudoku[(grid_x + _i) * 9 + grid_y + _j + 1], candidatos) then
+				removeItem(candidatos, sudoku[(grid_x + _i) * 9 + grid_y + _j + 1])
 			end
 		end
 	end
@@ -42,16 +52,17 @@ function gerarRandomNum(sudoku, index)
 	if #candidatos == 0 then
 		randomNum = -1
 	else
-		randomNum = math.random(#candidatos)
-		table.remove(candidatos, randomNum)
+		n = math.random(#candidatos)
+		randomNum = candidatos[n]
+		removeItem(candidatos, randomNum)
 	end
-	
+	print(randomNum)
 	return randomNum
 end
 
 function gerarBoard()
 	sudoku = {}
-	for i = 0, 81 do
+	for i = 1, 81 do
 		table.insert(sudoku, 0)
 	end
 
@@ -59,13 +70,19 @@ function gerarBoard()
 	while i < 81 do
 		randomNum = gerarRandomNum(sudoku, i)
 		if randomNum == -1 then
-			for i = 0, 81 do
+			for i = 1, 81 do
 				table.insert(sudoku, 0)
 			end
 			i = 0
 		else
-			sudoku[i] = randomNum
+			sudoku[i+1] = randomNum
 			i = i + 1
+		end
 	end
 	return sudoku
+end
+
+sudoku = gerarBoard()
+for i = 1, #sudoku, 9 do
+	print(sudoku[i] .. ' ' .. sudoku[i+1] .. ' ' .. sudoku[i+2] .. ' ' .. sudoku[i+3] .. ' ' .. sudoku[i+4] .. ' ' .. sudoku[i+5] .. ' ' .. sudoku[i+6] .. ' ' .. sudoku[i+7] .. ' ' .. sudoku[i+8])
 end
